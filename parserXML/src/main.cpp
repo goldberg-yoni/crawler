cc374b494d24d07cb622e1295b991f49c98b2098
Pierre Louchet <pierre.louchet@epitech.eu>
2015-09-03 18:22:47 +0200

    add of getopt for parserXML
    The string parameters such as "-3 * 4" are treated.

--- parserXML/src/main.cpp
+++ parserXML/src/main.cpp
@@ -1,15 +1,61 @@
 #include	<cstdlib>
+#include	<cstdio>
+#include	<getopt.h>
 #include	"parserXML.hh"
 
-int						main( int argc, char * * argv )
+int							main(int argc, char **argv)
 {
-	char *				expression = (argc > 1) ? argv[1] : NULL;
-	int					i;
-	
+	int						c;
+	int						option_index = 0;
+	static struct option	long_options[] =
+	{
+		{"file",	no_argument,		0,	'f'},
+		{0,			0,					0,	0}
+	};
+	char *					expression = NULL;
+	int						expression_index,
+							fileDone_index;
+
+	opterr = 0;
+
+	while ((c = getopt_long(argc, argv, "f", long_options, &option_index)) != -1)
+	{
+		switch (c)
+		{
+			case 0:
+				printf("0 %s\n", argv[optind]);
+				break;
+			case 'f':
+				printf("option f\n");
+				break;
+			case '?':
+				if (expression == NULL)
+				{
+					expression = argv[optind];
+					expression_index = optind;
+				}
+				else if (expression_index != optind && fileDone_index != optind)
+				{
+					fileDone_index = optind;
+					parserXML(expression, argv[optind]);
+				}
+				break;
+			default:
+				printf("?? getopt returned character code 0%o ??\n", c);
+		}
+	}
 	if (expression == NULL)
-		return (0);
-	for (i = 2; i < argc; i++)
-		parserXML(expression, argv[i]);
-	
-	return (0);
+		expression = (optind < argc) ? argv[optind++] : NULL;
+	if (expression == NULL)
+		return (EXIT_SUCCESS);
+	while (optind < argc)
+		if (fileDone_index != optind)
+		{
+			fileDone_index = optind;
+			parserXML(expression, argv[optind++]);
+		}
+		else
+			optind++;
+
+	return (EXIT_SUCCESS);
 }
--- parserXML/test/unitary_test.sh
+++ parserXML/test/unitary_test.sh
@@ -5,6 +5,6 @@
 #	2 : the XML file to test on
 #	3 : the unitary test file (containing the various expressions to test)
 # example :
-#	./test/unitary_test.sh ./bin/tiny.exe ./test/test.xml ./test/unitary_test | awk -F'\t' '{print $1,$2,$1 == $2}' | grep -n "0$"
+#	./test/unitary_test.sh ./bin/parserXML.exe ./test/test.xml ./test/unitary_test | awk -F'\t' '{print $1,$2,$1 == $2}' | grep -n "0$"
 
 awk -F '\t' '{OFS="";cmd="'$1' \"" $1 "\" '$2'";while (cmd | getline line){print "\"",line,"\"\t\"",$2,"\""};close(cmd)}' $3
