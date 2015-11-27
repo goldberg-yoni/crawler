b5d07f05f66317ef8d3542ad7e994a19e1cddd63
Hellfar <pierre.louchet@gmail.com>
2015-11-27 07:50:42 +0100

    Complete Crawler with draft redis managment.

--- src/Crawler.rb
+++ src/Crawler.rb
@@ -1,22 +1,62 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
-require 'mechanize'
 
-a = Mechanize.new { |agent|
+require 'set'
+require 'uri'
+
+require 'mechanize'
+require 'redis'
+
+agent = Mechanize.new { |agent|
 	agent.user_agent_alias = 'Mac Safari'
 }
+baseUrl = ARGV[0]
 
-a.get(ARGV[0]) do |page|
-	pp page.at('meta[name="description"]')[:content]
-	pp page.at('meta[name="keywords"]')[:content]
-	i = e = 0
-	page.links.each do |link|
-		# puts link.href
-		if link.href =~ URI::regexp
-			i += 1
+uncheckUrls = Set.new [baseUrl]
+urlPool = uncheckUrls.flatten
+
+redis = Redis.new
+if !(redis.ping == "PONG")
+	throw "The redis server weren't found (./redis-server &)."
+end
+
+i = 0
+while uncheckUrls.empty? === false
+	url = uncheckUrls.take 1
+	uncheckUrls.subtract url
+
+	url = url[0]
+	agent.get(url) do |page|
+		page.links.each do |t_link|
+			if t_link.href === nil
+				link = nil
+			elsif t_link.href =~ /\A#{URI::regexp(['http', 'https'])}\z/
+				link = t_link.href
+			else
+				link = url + t_link.href
+				if (link =~ /\A#{URI::regexp(['http', 'https'])}\z/) === nil
+					link = nil
+				end
+			end
+
+			if link != nil
+				link.slice! /\#(.)*/
+				if urlPool.add?( URI.parse( link ).to_s ) != nil
+					## only clean urls and not done goes here.
+					uncheckUrls.add link
+				end
+			end
+			pp link
 		end
-		e += 1
+
+		# redis.sadd "urlPool", url
+		# redis.sadd "item_" + i.to_s, url
+		# redis.sadd "item_" + i.to_s, page.at('meta[name="description"]')[:content]
+		# redis.sadd "item_" + i.to_s, page.at('meta[name="keywords"]')[:content]
+
+		i += 1
+
+		# pp urlPool
 	end
-	pp i.to_s + "/" + e.to_s
 end
