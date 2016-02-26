3aa0771fd7b7faf239eb7b689608154735d0f31b
Hellfar <pierre.louchet@gmail.com>
2016-02-26 14:19:53 +0100

    end merge

--- parserXML/inc/parserXML.hh
+++
@@ -1,8 +0,0 @@
-#pragma	once
-
-#ifndef		__PARSERXML_HH__
-# define	__PARSERXML_HH__
-
-int			parserXML( char const * const expression, char const * const filename );
-
-#endif
--- spider/inc/utils.h
+++
@@ -1,9 +0,0 @@
-#pragma		once
-
-#ifndef		__UTILS_H__
-# define	__UTILS_H__
-
-#define HAS(a, e)	(((a) & (e)) == (e))
-#define IS(a, e)	((a) == (e))
-
-#endif
